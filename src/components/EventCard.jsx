f3e258c2ce1707125f59a3808899993396dc32bc
lade6501 <lade6501@gmail.com>
2024-10-05 22:49:12 +0530

    feat issue#11: Added /events page

---
+++ src/components/EventCard.jsx
@@ -0,0 +1,40 @@
+import logo from '@/assets/images/logo.png';
+import PropTypes from 'prop-types';
+
+function EventCard({ eventDetails }) {
+  return (
+    <>
+      <div className="col-md-4 mb-4">
+        <div className="card shining-card  h-100">
+          <div className="event-date rounded-circle">
+            <div className="day">{eventDetails.event_date.day}</div>
+            <div className="month">{eventDetails.event_date.month}</div>
+          </div>
+          <img src={logo} className="event-image card-img-top" />
+          <div className="card-body text-center">
+            <h3 className="card-title">{eventDetails.event_title}</h3>
+            <p className="card-text">{eventDetails.event_description}</p>
+          </div>
+          <div className="card-footer text-center">
+            <button className="btn btn-primary">View Details</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+EventCard.propTypes = {
+  eventDetails: PropTypes.shape({
+    event_title: PropTypes.string,
+    event_date: PropTypes.shape({
+      day: PropTypes.string,
+      month: PropTypes.string,
+      year: PropTypes.string,
+    }),
+    event_description: PropTypes.string,
+    event_image: PropTypes.string,
+  }).isRequired,
+};
+
+export default EventCard;
